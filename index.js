54e25fd0912cd673527631cdc184aa018ff5e538
jiangmingwen <178202798@qq.com>
2019-10-29 09:48:38 +0800

    types tips

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
+"use strict";
+exports.__esModule = true;
 function add() {
-    var b = getPow(arguments);
-    var result = 0;
-    for (var num of arguments) {
-        result += num * (10 ** b);
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
     }
-    return result / (10 ** b);
+    var b = getPow.apply(void 0, args);
+    var result = 0;
+    for (var _a = 0, args_1 = args; _a < args_1.length; _a++) {
+        var num = args_1[_a];
+        result += num * (Math.pow(10, b));
+    }
+    return result / (Math.pow(10, b));
 }
-
 function sub() {
-    var b = getPow(arguments);
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    var b = getPow.apply(void 0, args);
     var result = 0;
     var i = 0;
-    for (var num of arguments) {
+    for (var _a = 0, args_2 = args; _a < args_2.length; _a++) {
+        var num = args_2[_a];
         if (i === 0) {
-            result = num * (10 ** b);
-        } else result -= num * (10 ** b);
+            result = num * (Math.pow(10, b));
+        }
+        else
+            result -= num * (Math.pow(10, b));
         i++;
     }
-    return result / (10 ** b);
+    return result / (Math.pow(10, b));
 }
-
 function multi() {
-    var b = getPow(arguments);
-    var result = 1;
-    for (var num of arguments) {
-        result *= num * (10 ** b);
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
     }
-    return result / (10 ** (b * arguments.length));
+    var b = getPow.apply(void 0, args);
+    var result = 1;
+    for (var _a = 0, args_3 = args; _a < args_3.length; _a++) {
+        var num = args_3[_a];
+        result *= num * (Math.pow(10, b));
+    }
+    return result / (Math.pow(10, (b * args.length)));
 }
-
 function div(num1, num2) {
-    if (!num2 || typeof num2 !== 'number') throw new Error("The denominator should be a number and cannot be zero");
+    if (!num2 || typeof num2 !== 'number')
+        throw new Error("The denominator should be a number and cannot be zero");
     var b = 0;
     var num1Arr = (Number(num1) || 0).toString().split('.');
     var num2Arr = (Number(num2) || 0).toString().split('.');
-    b = (num1Arr[1] || 0) > (num2Arr[1] || 0) ? (num1Arr[1] || 0) : (num2Arr[1] || 0);
-    return (num1 * (10 ** b)) / (num2 * (10 ** b));
+    b = (Number(num1Arr[1]) || 0) > (Number(num2Arr[1]) || 0) ? (Number(num1Arr[1]) || 0) : (Number(num2Arr[1]) || 0);
+    return (num1 * (Math.pow(10, b))) / (num2 * (Math.pow(10, b)));
 }
-
-function getPow(args) {
+function getPow() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
     var b = 0;
-    for (var num of args) {
-        if (num === 'undefined' || num === null) {
+    for (var _a = 0, args_4 = args; _a < args_4.length; _a++) {
+        var num = args_4[_a];
+        if (num === undefined || num === null) {
             console.warn("operation param is error");
         }
         var numStr = (Number(num) || 0).toString();
@@ -52,10 +73,10 @@ function getPow(args) {
     }
     return b;
 }
-
-module.exports= {
-    add,
-    sub,
-    multi,
-    div
-}
+var Decimal = {
+    add: add,
+    sub: sub,
+    multi: multi,
+    div: div
+};
+exports["default"] = Decimal;
---
+++ index.ts
@@ -0,0 +1,64 @@
+
+function add(...args:number[]): number {
+    var b = getPow(...args);
+    var result = 0;
+    for (var num of args) {
+        result += num * (10 ** b);
+    }
+    return result / (10 ** b);
+}
+
+function sub(...args:number[]): number {
+    var b = getPow(...args);
+    var result = 0;
+    var i = 0;
+    for (var num of args) {
+        if (i === 0) {
+            result = num * (10 ** b);
+        } else result -= num * (10 ** b);
+        i++;
+    }
+    return result / (10 ** b);
+}
+
+function multi(...args:number[]): number {
+    var b = getPow(...args);
+    var result = 1;
+    for (var num of args) {
+        result *= num * (10 ** b);
+    }
+    return result / (10 ** (b * args.length));
+}
+
+function div(num1: number, num2: number): number {
+    if (!num2 || typeof num2 !== 'number') throw new Error("The denominator should be a number and cannot be zero");
+    var b = 0;
+    var num1Arr = (Number(num1) || 0).toString().split('.');
+    var num2Arr = (Number(num2) || 0).toString().split('.');
+    b = (Number(num1Arr[1]) || 0) > (Number(num2Arr[1]) || 0) ? (Number(num1Arr[1]) || 0) : (Number(num2Arr[1]) || 0);
+    return (num1 * (10 ** b)) / (num2 * (10 ** b));
+}
+
+function getPow(...args:number[]): number {
+    var b = 0;
+    for (var num of args) {
+        if (num === undefined || num === null) {
+            console.warn("operation param is error");
+        }
+        var numStr = (Number(num) || 0).toString();
+        var numArr = numStr.split('.');
+        if (numArr.length == 2 && numArr[1].length > b) {
+            b = numArr[1].length;
+        }
+    }
+    return b;
+}
+
+const Decimal = {
+    add,
+    sub,
+    multi,
+    div
+}
+
+export default Decimal;
